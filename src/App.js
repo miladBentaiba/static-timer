1fe4e02e865fae84060fb52143ab1c526b4a47c6
miladBentaiba <bm_bentaiba@esi.dz>
2019-05-15 09:36:13 +0100

    create static timer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="parent">
+      <div className="timer">
+        <p className="time">00</p>
+        <p className="time">:</p>
+        <p className="time">30</p>
+        <p className="time">:</p>
+        <p className="time">00</p>
+      </div>
+      <div className="labels">
+        <p className="label">Hour</p>
+        <p className="label"></p>
+        <p className="label">Minute</p>
+        <p className="label"></p>
+        <p className="label">Second</p>
+      </div>
     </div>
   );
 }
